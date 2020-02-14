d36c7847af0bb4583f31f630aa977e4095b31609
Sasha178 <you@shurik90_08@mail.ru>
2020-02-14 23:43:02 +0300

    Crushing into components Profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img className='picture_content'
+                     src='https://lh3.googleusercontent.com/proxy/9zhrl5O9k4AAqOE8GE-p1b7nuSgWy84dfw3pssZj0BrtVtFaF7IF4fGxi3O_PhzJVgNB3artHd0V3Ub6zUKsMgVEI5BYXjzVy_lC9BhRW5nmepnixYq5ET4v5oOczRERoKn3DXuTBQr2dD_u8-6o2papdrH3T4s3jc-WsNqLlQ'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                MY POST
+                <div>
+                    NEW POST
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
