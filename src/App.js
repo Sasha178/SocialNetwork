34203c1615d3551138814e9a579b415f85c9718a
Sasha178 <you@shurik90_08@mail.ru>
2020-02-14 23:55:53 +0300

    Error handling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
         <div className='app_wrapper'>
             <Header/>
             <Navbar/>
-            <Navbar/>
+            <Profile/>
         </div>);
 }
 
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Navbar = () => {
+const Profile = () => {
     return (
         <div className='content'>
             <div>
                 <img className='picture_content'
-                     src='https://lh3.googleusercontent.com/proxy/9zhrl5O9k4AAqOE8GE-p1b7nuSgWy84dfw3pssZj0BrtVtFaF7IF4fGxi3O_PhzJVgNB3artHd0V3Ub6zUKsMgVEI5BYXjzVy_lC9BhRW5nmepnixYq5ET4v5oOczRERoKn3DXuTBQr2dD_u8-6o2papdrH3T4s3jc-WsNqLlQ'/>
+                     src='https://klike.net/uploads/posts/2019-01/medium/1547794995_3.jpg'/>
             </div>
             <div>
                 ava + description
@@ -26,4 +26,4 @@ const Navbar = () => {
     )
 };
 
-export default Navbar;
+export default Profile;
