9db809d749f6acabec27c4f02c000c5b9ba0d15c
Sasha178 <you@shurik90_08@mail.ru>
2020-02-14 23:34:30 +0300

    Crushing into components Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+
+            <header className='header'>
+                <img className='logo_img'
+                     src='https://avatanplus.com/files/resources/mid/5ac635924849216296414378.png'/>
+            </header>
+            )
+            };
+
+export default Header;
