47ef8448fb9f960adb9c66c707947e3e0542fb6b
Sasha178 <you@shurik90_08@mail.ru>
2020-02-16 21:38:08 +0300

    Accessing and rendering a props object

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img
                 src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzhofvCZtJaJ8H9GwGqGBnaL3We2NfHrkam_DMxyk8gVUmWQA7'/>
-            post 1
+            {props.message}
             <div>
                 <span>Like</span>
             </div>
