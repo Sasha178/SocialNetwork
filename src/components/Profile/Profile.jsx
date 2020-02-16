854ab38dd1c5ef61aa7c87d39431f67d24529322
Sasha178 <you@shurik90_08@mail.ru>
2020-02-17 01:27:47 +0300

    Adding styles to the app component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img className={s.picture_content}
                      src='https://klike.net/uploads/posts/2019-01/medium/1547794995_3.jpg'/>
