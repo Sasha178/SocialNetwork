a2707bda7a759e9d2c7b7721b9f328104331c3f1
Sasha178 <you@shurik90_08@mail.ru>
2020-02-24 01:14:27 +0300

    Adding components Settings

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Settings = (props) => {
+    return (
+        <div>
+            Settings
+            <div>
+                <p>Настройка доступа</p>
+                <p>Кто может мне писать</p>
+            </div>
+            <div>
+                <button>Сохранить</button>
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
