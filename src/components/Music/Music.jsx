4b7b78e8a0cc9fc97523df07c9dc819c9b3184e1
Sasha178 <you@shurik90_08@mail.ru>
2020-02-24 01:11:48 +0300

    Adding components Music

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Music = (props) => {
+    return (
+        <div>
+            Music
+            <div>
+                Play list
+            </div>
+            <div>
+                New name.mp3
+            </div>
+        </div>
+    )
+}
+
+export default Music;
