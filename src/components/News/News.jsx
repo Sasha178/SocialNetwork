7b259e5fc2ce12387903ea4ea6feac25fd3e241a
Sasha178 <you@shurik90_08@mail.ru>
2020-02-24 01:13:58 +0300

    Adding components News

---
+++ src/components/News/News.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+const News = (props) => {
+    return (
+        <div>
+            New post list
+            <div>
+                new post
+            </div>
+            <div>
+                new post
+            </div>
+            <div>
+                new post
+            </div>
+            <div>
+                new post
+            </div>
+            <div>
+                new post
+            </div>
+        </div>
+    )
+}
+
+export default News;
