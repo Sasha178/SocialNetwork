c1ef0fb4f9e4fa9bf222e3ca1eec5093b6d7565b
Sasha178 <you@shurik90_08@mail.ru>
2020-02-27 00:22:14 +0300

    Package upgrade

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route } from 'react-router-dom';
 import Settings from "./components/Settings/Settings";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
+import { BrowserRouter, Route } from "react-router-dom";
 
 
 const App = () => {
     return (
-        <BrowserRouter>
+       <BrowserRouter>
             <div className='app_wrapper'>
                 <Header/>
                 <Navbar/>
@@ -24,7 +24,7 @@ const App = () => {
                     <Route path='/settings' component={Settings}/>
                 </div>
             </div>
-        </BrowserRouter>
+       </BrowserRouter>
     );
 }
 
